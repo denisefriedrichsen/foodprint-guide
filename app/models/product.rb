9b1e5dba181a570417e5e6a76f82ee9cd55fda42
denise friedrichsen <denise.friedrichsen@yahoo.de>
2020-03-03 12:54:11 +0100

    Added the offerings model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :category, inclusion: {in: %w(fruits vegetables cereals dairy meat)}
+  has_many :offerings
+  has_many :producers, through: :offerings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_104619) do
+ActiveRecord::Schema.define(version: 2020_03_03_114242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offerings", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "producer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["producer_id"], name: "index_offerings_on_producer_id"
+    t.index ["product_id"], name: "index_offerings_on_product_id"
+  end
 
   create_table "producers", force: :cascade do |t|
     t.string "company_name"
@@ -48,4 +57,6 @@ ActiveRecord::Schema.define(version: 2020_03_03_104619) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offerings", "producers"
+  add_foreign_key "offerings", "products"
 end
