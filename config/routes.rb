a6b767c08a94a10b8068805e7729618d528918b8
denise friedrichsen <denise.friedrichsen@yahoo.de>
2020-03-05 20:49:57 +0100

    Added pages for favourite products & producers

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :favourite_products, only: [:create, :index, :destroy]
+  end
+
+  resources :producers, only: [:show] do
+    resources :favourite_producers, only: [:create, :index, :destroy]
+  end
+
   resources :posts, only: [:index, :show]
-  resources :producers, only: [:show]
-  resources :favourite_producers, only: [:create, :index, :destroy]
-  resources :favourite_products, only: [:create, :index, :destroy]
   # resources :profiles, only: [:edit, :update]
   # patch '/users/update', to: 'profiles#update', as: :update_user
   # get '/profile/edit', to: 'profiles#edit', as: :edit_user
