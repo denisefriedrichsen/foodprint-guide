f1e4c26d212b7416fd3154b7e19be5069f841bf5
Verena Mueller <verena.mller@gmail.com>
2020-03-05 11:19:44 +0100

    added geocode gem and lat + lang to Producers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_153032) do
+ActiveRecord::Schema.define(version: 2020_03_05_101422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 2020_03_04_153032) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "products", force: :cascade do |t|
