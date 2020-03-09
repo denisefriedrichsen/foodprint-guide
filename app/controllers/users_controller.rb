cfbff581caa63a6bcf9a09dce192f10fe322c24e
Flo Leinenkugel <flolein@gmx.net>
2020-03-09 17:50:32 +0100

    refactored favourite producer/method, indizes still ugly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,26 +5,18 @@ class UsersController < ApplicationController
   def index_product
     @votes = @user.find_liked_items
     @products = @votes.filter { |item| item.class.to_s == "Product"}
-
-    #@votes = @user.votes.where(votable_type: "Product")
   end
 
   def index_producer
      @votes = @user.find_liked_items
     @producers = @votes.filter { |item| item.class.to_s == "Producer"}
-    #@votes = @user.votes.where(votable_type: "Producer")
   end
 
-  # def show
-  #   @vote = User.votes(:product_id])
-  # end
-
-
   private
+
   def set_user
     @user = current_user
   end
-
 
 end
 
