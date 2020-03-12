35f9d47a6640c4fcb930ec38085dba9890d3f077
Flo Leinenkugel <flolein@gmx.net>
2020-03-12 14:22:38 +0100

    fixed producerfavereload

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
 
@@ -8,10 +9,11 @@ Rails.application.routes.draw do
   resources :producers, only: [:show]
 
   resources :posts, only: [:index, :show]
+
   resources :users
 
-    get 'favourite_products' => 'users#index_product', as: :favourite_products
-    get 'favourite_producers' => 'users#index_producer', as: :favourite_producers
+  get 'favourite_products' => 'users#index_product', as: :favourite_products
+  get 'favourite_producers' => 'users#index_producer', as: :favourite_producers
   patch 'product/:id/upvote' => 'products#upvote', as: :product_upvote
   patch 'product/:id/downvote' => 'products#downvote', as: :product_downvote
 
