38629c9e64607625b43b580894165a35d5ad2bff
Marian Bohndick <marian.bohndick@gmail.com>
2020-03-12 15:44:54 +0100

    don't merge! - worked on filter for favorite produces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,38 @@ class UsersController < ApplicationController
     @products = @votes.filter { |item| item.class.to_s == "Product"}
   end
 
+  # def index_producer
+  #   @navbar_favourite_producer = true
+  #   @votes = @user.find_liked_items
+  #   @producers = @votes.filter { |item| item.class.to_s == "Producer"}
+
+    # @filtered_producers = Vote.joins(:offerings).where(offerings: { producer_id: @producer.id })
+
+    # Producer.joins(:offerings).where(offerings: { producer_id: @producer.id })
+
+
   def index_producer
     @navbar_favourite_producer = true
-    @votes = @user.find_liked_items
-    @producers = @votes.filter { |item| item.class.to_s == "Producer"}
+    @votes = @user.votes.where(votable_type: "Producer")
+    producer_ids = @votes.map(&:votable_id)
+    @producers = Producer.where(id: producer_ids)
+
+    categories = ["meat", "vegetables", "fruits"]
+
+    @producers = @producers.joins(offerings: :product).where(offerings: { products: { category: categories } }).distinct
+
+
+
+    @markers = @producers.map do |producer|
+      {
+        lat: producer.latitude,
+        lng: producer.longitude,
+        infoWindow: render_to_string(partial: 'products/info_window', locals: { producer: producer }),
+        image_url: helpers.asset_url('marker.svg')
+      }
+
+    end
+
 
     @markers = @producers.map do |producer|
       {
